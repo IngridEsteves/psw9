90e42c52c9af5fa06413296e0e7369b77db5027d
Ingrid Esteves <ingridestevespereiraiep@gmail.com>
2024-02-20 17:30:37 -0300

    cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
 
 # Create your views here.
+def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    else:
+        nome = request.POST.get('nome')
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.WARNING, 'As senhas não coincidem')
+            return redirect('/usuarios/cadastro')
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.WARNING, 'Já existe um usuário com o mesmo username')
+            return redirect('/usuarios/cadastro')
+
+        try:
+            user = User.objects.create_user(
+                first_name=nome,
+                username=username,
+                password=confirmar_senha,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
+            return redirect('/usuarios/login')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
+            return redirect('/usuarios/cadastro')
