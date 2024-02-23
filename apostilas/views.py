09d8629dd40d4d7ea4b8180d6954103bf2856867
Ingrid Esteves <ingridestevespereiraiep@gmail.com>
2024-02-22 21:41:30 -0300

    apostila

--- apostilas/views.py
+++ apostilas/views.py
@@ -8,6 +8,7 @@ from django.contrib.messages import constants
 def adicionar_apostilas(request):
     if request.method == 'GET':
         apostilas = Apostila.objects.filter(user=request.user)
+        # TODO: Criar as tags
         views_totais = ViewApostila.objects.filter(apostila__user=request.user).count()
         return render(request, 'adicionar_apostilas.html', {'apostilas': apostilas, 'views_totais': views_totais})
     elif request.method == 'POST':
@@ -19,4 +20,24 @@ def adicionar_apostilas(request):
         messages.add_message(
             request, constants.SUCCESS, 'Apostila adicionada com sucesso.'
         )
+
+        # TODO: Fazer o ranking
+
         return redirect('/apostilas/adicionar_apostilas/')
+
+
+def apostila(request, id):
+    apostila = Apostila.objects.get(id=id)
+
+    view = ViewApostila(
+        ip=request.META['REMOTE_ADDR'],
+        apostila=apostila
+    )
+    view.save()
+
+    views_unicas = ViewApostila.objects.filter(apostila=apostila).values('ip').distinct().count()
+    views_totais = ViewApostila.objects.filter(apostila=apostila).count()
+
+    return render(request, 'apostila.html', {'apostila': apostila, 'views_unicas': views_unicas, 'views_totais': views_totais, })
+
+    # TODO: Avaliação das apostilas
